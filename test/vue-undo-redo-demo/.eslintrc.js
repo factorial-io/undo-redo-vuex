5e16cf0a569ccf1588f7a199308837a3d56d8585
Andrew Beng <madbeng@gmail.com>
2018-05-08 23:59:13 +0200

    Include demo vuejs app with eslint/prettier/vue config

---
+++ test/vue-undo-redo-demo/.eslintrc.js
@@ -0,0 +1,44 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:vue/base'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'prettier',
+    'vue'
+  ],
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // prettier
+    'prettier/prettier': [
+      2,
+      {
+        trailingComma: 'all',
+        singleQuote: true
+      }
+    ]
+  }
+}
