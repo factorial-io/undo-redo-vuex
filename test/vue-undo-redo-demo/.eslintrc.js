665d8536b377512e6c1a63c96f1fece0774e36a6
Andrew Beng <andrewbeng89@gmail.com>
2018-05-22 18:32:31 +0200

    stylelint and eslint deconflict with prettier

--- test/vue-undo-redo-demo/.eslintrc.js
+++ test/vue-undo-redo-demo/.eslintrc.js
@@ -32,13 +32,5 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // prettier
-    'prettier/prettier': [
-      2,
-      {
-        trailingComma: 'all',
-        singleQuote: true
-      }
-    ]
   }
 }
