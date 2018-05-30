84b31af93e5008e270278dcb8dcf80060d56e535
Andrew Beng <andrewbeng89@gmail.com>
2018-05-30 19:43:36 +0200

    Scaffold store and module for testing

---
+++ test/store/index.js
@@ -0,0 +1,25 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import list from './modules/list';
+import undoRedo from '../../';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+export default new Vuex.Store({
+  plugins: [
+    undoRedo({
+      paths: [
+        {
+          namespace: 'list',
+          ignoreMutations: ['addShadow', 'removeShadow'],
+        },
+      ],
+    }),
+  ],
+  modules: {
+    list,
+  },
+  strict: debug,
+});
