e171ca6877cd402cbd9b20323ce842b20ad43598
Andrew Beng <andrewbeng89@gmail.com>
2018-09-24 17:11:22 +0200

    WIP switch tests to typescript

---
+++ tests/store/modules/list.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-param-reassign, no-shadow */
+import deepEqual from 'fast-deep-equal';
+import { scaffoldStore } from '../../../src/undoRedo';
+
+const state = {
+  list: [],
+  shadow: [],
+};
+
+const getters = {
+  getList: ({ list }: { list: Array<any> }) => list,
+  getItem: (state: any) => ({ item }: { item: any }) => state.list.find((i: any) => deepEqual(i, item)),
+  getShadow: ({ shadow }: { shadow: Array<any> }) => shadow,
+};
+
+interface Context {
+  commit: Function,
+  state: any,
+  getters: any,
+  rootState: any,
+  rootGetters: any
+}
+
+interface Payload {
+  index?: number,
+  item?: any
+}
+
+const actions = {
+  // NB: add/remove shadow actions to test undo/redo callback actions
+  addShadow({ commit }: Context, { item }: Payload) {
+    commit('addShadow', { item });
+  },
+  removeShadow({ commit }: Context, { index }: Payload) {
+    commit('removeShadow', { index });
+  },
+};
+
+export const mutations = {
+  emptyState: (state: any) => {
+    state.list = [];
+  },
+  addItem: (state: any, { item }: { item: any }) => {
+    state.list = [...state.list, item];
+  },
+  updateItem: (state: any, { item, index }: Payload) => {
+    state.list.splice(index, 1, item);
+  },
+  removeItem: (state: any, { index }: Payload) => {
+    state.list.splice(index, 1);
+  },
+  addShadow: (state: any, { item }: Payload) => {
+    state.shadow = [...state.shadow, item];
+  },
+  removeShadow: (state: any, { index }: Payload) => {
+    state.shadow.splice(index, 1);
+  },
+};
+
+export default scaffoldStore({
+  state,
+  getters,
+  actions,
+  mutations,
+  namespaced: true,
+});
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,6 +1,17 @@
-describe("First test", () => {
-  it("will pass", () => {
-    const msg = "new message";
-    expect("new message").toMatch(msg);
+import { mutations } from "../store/modules/list";
+
+const item = {
+  foo: 'bar',
+};
+
+describe("Simple testing for undo/redo on a namespaced vuex store", () => {
+  it("Add item to list", () => {
+    const state = { list: [] };
+    const expectedState = [{ ...item }];
+
+    // Commit the item to the store and assert
+    mutations.addItem(state, { item });
+    
+    expect(state.list).toEqual(expectedState);
   });
 });
