4433e67487dbad0f5b5baaae797a1f78e8795939
Andrew Beng <andrewbeng89@gmail.com>
2018-09-27 17:10:25 +0200

    stylelint fixes

--- tests/vue-undo-redo-demo/src/components/HelloWorld.vue
+++ tests/vue-undo-redo-demo/src/components/HelloWorld.vue
@@ -50,14 +50,17 @@ export default Vue.extend({
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
   padding: 0;
+  list-style-type: none;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
