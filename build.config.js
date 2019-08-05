a279347f582d1b29b9955b589c30b64097362718
Andrew Beng <andrewbeng89@gmail.com>
2019-08-05 10:02:50 +0200

    Create dedicated webpack build config

---
+++ build.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/undoRedo.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "undo-redo-vuex.min.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new CleanWebpackPlugin()],
+  mode: "production"
+};
