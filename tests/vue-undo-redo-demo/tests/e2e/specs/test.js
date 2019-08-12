49b664100fe4532ca5ba56d9cb4ec11587833612
Andrew Beng <andrewbeng89@gmail.com>
2019-08-12 18:16:49 +0200

    Update E2E script with mock login

--- tests/vue-undo-redo-demo/tests/e2e/specs/test.js
+++ tests/vue-undo-redo-demo/tests/e2e/specs/test.js
@@ -1,22 +1,23 @@
 // https://docs.cypress.io/api/introduction/api.html
 // NB: https://github.com/vuejs/vuex/issues/1220 and https://github.com/vuejs/vuex/pull/1121
-// ERROR: Vuex/Typescript mappers issue: property does not exist on type 'CombinedVueInstance<Vue, ...>' 
+// ERROR: Vuex/Typescript mappers issue: property does not exist on type 'CombinedVueInstance<Vue, ...>'
 
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
+    cy.contains("h1", "NOT Logged In");
+    cy.get("button").click();
+    cy.contains("h1", "Logged In");
     cy.contains("h4", "You don't have any Todos yet");
     cy.get("#undo").should("be.disabled");
     cy.get("#redo").should("be.disabled");
   });
 
   it("Type new todo", () => {
-    cy.get("input")
-      .type("Hello, world!{enter}");
-      cy.get("#undo").should("not.be.disabled");
-    
-    cy.get("ol")
-      .contains("li", "Hello, world!");
+    cy.get("input").type("Hello, world!{enter}");
+    cy.get("#undo").should("not.be.disabled");
+
+    cy.get("ol").contains("li", "Hello, world!");
   });
 
   it("Undo removes todo", () => {
@@ -28,8 +29,7 @@ describe("My First Test", () => {
 
   it("Redo input", () => {
     cy.get("#redo").click();
-    cy.get("ol")
-      .contains("li", "Hello, world!");
+    cy.get("ol").contains("li", "Hello, world!");
     cy.get("#undo").should("not.be.disabled");
     cy.get("#redo").should("be.disabled");
   });
