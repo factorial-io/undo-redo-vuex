36870f239059c6296ab0706fae28e49dc8728402
Andrew Beng <andrewbeng89@gmail.com>
2019-08-27 16:11:21 +0200

    #7 implement the clear function

---
+++ src/clear.ts
@@ -0,0 +1,39 @@
+import { EMPTY_STATE } from "./constants";
+import {
+  getConfig,
+  pipeActions,
+  setConfig,
+  updateCanUndoRedo
+} from "./utils-undo-redo";
+
+export default ({
+  paths,
+  store
+}: {
+  paths: UndoRedoOptions[];
+  store: any;
+}) => async (namespace: string) => {
+  const config = getConfig(paths)(namespace);
+
+  if (Object.keys(config).length) {
+    const undoCallbacks = config.done.map(({ payload }: { payload: any }) => ({
+      action: payload.undoCallback ? `${namespace}${payload.undoCallback}` : "",
+      payload
+    }));
+    await pipeActions(store)(undoCallbacks);
+
+    const done: [] = [];
+    const undone: [] = [];
+    config.newMutation = false;
+    store.commit(`${namespace}${EMPTY_STATE}`);
+
+    config.newMutation = true;
+    setConfig(paths)(namespace, {
+      ...config,
+      done,
+      undone
+    });
+
+    updateCanUndoRedo({ paths, store })(namespace);
+  }
+};
